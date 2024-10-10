ec0f74e660e45c273d92a7fa52e0ba2c81e1118e
Matheus Viscki <matheusviscki@gmail.com>
2024-10-09 21:39:21 -0300

    feat: criar primeiro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from "./components/new-component/new-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponentComponent],
   template: `
     <router-outlet />
+    <app-new-component />
   `,
 })
 export class AppComponent {}
