da520eb4442a99bd18a17cad578db9f98a6a84ef
Matheus Viscki <matheusviscki@gmail.com>
2024-10-09 22:07:21 -0300

    feat: criar componente para praticar templates do angular - text interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { NewComponentComponent } from "./components/new-component/new-component.component";
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponentComponent],
+  imports: [RouterOutlet, NewComponentComponent, TemplateBindingComponent],
   template: `
     <router-outlet />
     <app-new-component />
+    <app-template-binding />
   `,
 })
 export class AppComponent {}
