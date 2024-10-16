eb2edd428b8d61c4b0fc9f56591852a12bf5d537
Matheus Viscki <matheusviscki@gmail.com>
2024-10-16 20:33:50 -0300

    feat: aplicar conceitos de signal() - effect: semelhante com userEffect do react

--- src/app/components/signals/signals/signals.component.ts
+++ src/app/components/signals/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -16,7 +16,26 @@ export class SignalsComponent {
     return `Full name: ${this.firstName()} ${this.lastName()}`;
   });
 
-  constructor() {}
+    /*
+      effect - raramente são necessários na maioria dos códigos,
+      mas podem ser úteis em circunstâncias específicas.
+      - Registro de dados sendo exibidos e quando eles mudam, seja para análise
+        ou como ferramenta de depuração.
+
+      - Manter os dados sincronizados com o window.localStorage.
+
+      - Adicionando comportamento DOM personalizado que não pode ser expresso com
+        sintaxe de modelo.
+
+      - Executar renderização personalizada em um <canvas>, biblioteca de gráficos
+        ou outra biblioteca de UI de terceiros.
+    */
+
+  constructor() {
+    effect(() => {
+      console.log(this.fullName())
+    });
+  }
 
   public updateName() {
     this.firstName.set("Igor");
