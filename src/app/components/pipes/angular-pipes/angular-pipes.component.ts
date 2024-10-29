4fa18d84faed40c72aead4427146f93de887993f
Matheus Viscki <matheusvisckI@gmail.com>
2024-10-29 20:11:06 -0300

    feat: utilizar pipes para formatação monetaria usando a locale atual.

--- src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,13 +1,20 @@
-import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, PercentPipe, registerLocaleData, UpperCasePipe } from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
+
+import LocalePt from '@angular/common/locales/pt';
+registerLocaleData(LocalePt);
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, JsonPipe, AsyncPipe],
+  imports: [DatePipe, UpperCasePipe, JsonPipe, AsyncPipe, CurrencyPipe, DecimalPipe, PercentPipe],
   templateUrl: './angular-pipes.component.html',
-  styleUrl: './angular-pipes.component.scss'
+  styleUrl: './angular-pipes.component.scss',
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR',
+  }]
 })
 export class AngularPipesComponent {
   public date = signal(new Date());
