63041c9a2e50d8aa028ad7280f1d60be2376c634
Matheus Viscki <matheusvisckI@gmail.com>
2024-10-29 22:49:10 -0300

    feat: criar custom validators

--- src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,10 +1,21 @@
+import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+
+function textValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    if (control.value === 'Matheus') {
+      return null;
+    }
+
+    return { invalidText: true };
+  }
+}
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.scss'
 })
@@ -14,7 +25,7 @@ export class ReactiveFormsComponent {
   // constructor(private _fb: FormBuilder) {}
 
   public profileForm = this.#fb.group({
-    name: ["", Validators.minLength(5)],
+    name: ["", [Validators.minLength(5), textValidator()]],
     myStacks: this.#fb.group({
       front: ['Angular'],
       back: ['Node.js']
