453ad5632811c54c9c19ff4bc6c5919b2a8327ab
Matheus Viscki <matheusvisckI@gmail.com>
2024-10-30 08:02:14 -0300

    feat: configurar schematics e tsconfig
    - criar alias para importação
    - adicionar configurações no schematics o criar um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { NewComponentComponent } from "./components/new-component/new-component.component";
-import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
-import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
-import { TemplateDeferrableViewsComponent } from "./components/template/template-deferrable-views/template-deferrable-views.component";
-import { SignalsComponent } from "./components/signals/signals/signals.component";
-import { PaiOuMaeComponent } from "./components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component";
-import { AngularPipesComponent } from "./components/pipes/angular-pipes/angular-pipes.component";
-import { TemplateDrivenFormsComponent } from "./components/forms/template-driven-forms/template-driven-forms.component";
-import { ReactiveFormsComponent } from "./components/forms/reactive-forms/reactive-forms.component";
-import { ContentComponent } from "./components/content/content.component";
-import { HostElementsComponent } from "./components/host-elements/host-elements.component";
-import { LifeCycleComponent } from "./components/life-cycle/life-cycle.component";
+import { NewComponentComponent } from "@components/new-component/new-component.component";
+import { TemplateBindingComponent } from '@components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from "@components/template/template-variables/template-variables.component";
+import { TemplateDeferrableViewsComponent } from "@components/template/template-deferrable-views/template-deferrable-views.component";
+import { SignalsComponent } from "@components/signals/signals/signals.component";
+import { PaiOuMaeComponent } from "@components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component";
+import { AngularPipesComponent } from "@components/pipes/angular-pipes/angular-pipes.component";
+import { TemplateDrivenFormsComponent } from "@components/forms/template-driven-forms/template-driven-forms.component";
+import { ReactiveFormsComponent } from "@components/forms/reactive-forms/reactive-forms.component";
+import { ContentComponent } from "@components/content/content.component";
+import { HostElementsComponent } from "@components/host-elements/host-elements.component";
+import { LifeCycleComponent } from "@components/life-cycle/life-cycle.component";
 
 @Component({
   selector: 'app-root',
