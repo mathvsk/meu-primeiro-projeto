144cecaa8bb92f5b35f608e7da8a68073cfba87b
Matheus Viscki <matheusvisckI@gmail.com>
2024-10-29 21:04:08 -0300

    feat: aplicar conceitos de formulario
    - Template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import { TemplateDeferrableViewsComponent } from "./components/template/template
 import { SignalsComponent } from "./components/signals/signals/signals.component";
 import { PaiOuMaeComponent } from "./components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component";
 import { AngularPipesComponent } from "./components/pipes/angular-pipes/angular-pipes.component";
+import { TemplateDrivenFormsComponent } from "./components/forms/template-driven-forms/template-driven-forms.component";
+import { ReactiveFormsComponent } from "./components/forms/reactive-forms/reactive-forms.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponentComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateDeferrableViewsComponent, SignalsComponent, PaiOuMaeComponent, AngularPipesComponent],
+  imports: [RouterOutlet, NewComponentComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateDeferrableViewsComponent, SignalsComponent, PaiOuMaeComponent, AngularPipesComponent, TemplateDrivenFormsComponent, ReactiveFormsComponent],
   template: `
     <router-outlet />
     <!-- <app-new-component />
@@ -21,7 +23,9 @@ import { AngularPipesComponent } from "./components/pipes/angular-pipes/angular-
     <!-- <app-template-deferrable-views /> -->
     <!-- <app-signals /> -->
     <!-- <app-pai-ou-mae /> -->
-     <app-angular-pipes />
+    <!-- <app-angular-pipes /> -->
+    <app-template-driven-forms />
+    <!-- <app-reactive-forms /> -->
   `,
 })
 export class AppComponent {}
