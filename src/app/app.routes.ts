14d39f158f6a4ce330c87bd9b2ffe1136ec92316
Matheus Viscki <matheusvisckI@gmail.com>
2024-10-31 23:01:10 -0300

    feat: adicionar rotas filhas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,63 +19,77 @@ import { TemplateVariablesComponent } from '@components/template/template-variab
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        children: [
+            {
+                path: '',
+                component: HomeComponent
+            },
+            {
+                path: 'app-new-component',
+                component: NewComponentComponent
+            },
+            {
+                path: 'app-template-binding',
+                component: TemplateBindingComponent
+            },
+            {
+                path: 'app-template-variables',
+                component: TemplateVariablesComponent
+            },
+            {
+                path: 'app-template-deferrable-views',
+                component: TemplateDeferrableViewsComponent
+            },
+            {
+                path: 'app-signals',
+                component: SignalsComponent
+            },
+            {
+                path: 'app-pai-ou-mae',
+                component: PaiOuMaeComponent
+            },
+            {
+                path: 'app-angular-pipes',
+                component: AngularPipesComponent
+            },
+            {
+                path: 'app-template-driven-forms',
+                component: TemplateDrivenFormsComponent
+            },
+            {
+                path: 'app-rective-forms',
+                component: ReactiveFormsComponent
+            },
+            {
+                path: 'app-content-wrapper',
+                component: ContentWrapperComponent
+            },
+            {
+                path: 'app-host-elements',
+                component: HostElementsComponent
+            },
+            {
+                path: 'app-life-cycle-wrapper',
+                component: LifeCycleWrapperComponent
+            },
+            {
+                path: 'app-consume-service',
+                component: ConsumeServiceComponent
+            },
+            {
+                path: 'app-rota-com-parametro/:id',
+                component: RotaComParametroComponent
+            },
+        ]
     },
     {
-        path: 'app-new-component',
-        component: NewComponentComponent
-    },
-    {
-        path: 'app-template-binding',
-        component: TemplateBindingComponent
-    },
-    {
-        path: 'app-template-variables',
-        component: TemplateVariablesComponent
-    },
-    {
-        path: 'app-template-deferrable-views',
-        component: TemplateDeferrableViewsComponent
-    },
-    {
-        path: 'app-signals',
-        component: SignalsComponent
-    },
-    {
-        path: 'app-pai-ou-mae',
-        component: PaiOuMaeComponent
-    },
-    {
-        path: 'app-angular-pipes',
-        component: AngularPipesComponent
-    },
-    {
-        path: 'app-template-driven-forms',
-        component: TemplateDrivenFormsComponent
-    },
-    {
-        path: 'app-rective-forms',
-        component: ReactiveFormsComponent
-    },
-    {
-        path: 'app-content-wrapper',
-        component: ContentWrapperComponent
-    },
-    {
-        path: 'app-host-elements',
-        component: HostElementsComponent
-    },
-    {
-        path: 'app-life-cycle-wrapper',
-        component: LifeCycleWrapperComponent
-    },
-    {
-        path: 'app-consume-service',
-        component: ConsumeServiceComponent
-    },
-    {
-        path: 'app-rota-com-parametro/:id',
-        component: RotaComParametroComponent
+        path: 'admin',
+        children: [
+            {
+                path: 'dashboard',
+                component: NewComponentComponent
+            }
+        ]
     },
     {
         path: '**', //rota coringa
